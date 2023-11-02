42610ec2b4b9b8b5637ea724bf245c74fd68ed84
Donald <donald@hyper-cloud.kr>
2023-11-02 15:27:54 +0900

    test3

--- src/Component.jsx
+++ src/Component.jsx
@@ -17,7 +17,7 @@ export default function Component() {
     }
 
     const showARViewContents = () => {
-        window.Android.moveARViewContents()
+        window.Android.checkPermission()
     }
 
 
